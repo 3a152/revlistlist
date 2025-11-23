5275937995261809a4fc4891011f1e459c204031
3a152 <uwuekittenslayer@gmail.com>
2025-11-23 08:03:45 +0700

    added back the finobe aka old novarin css by gladosb5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,19 @@
 <link href="https://ycb.glados.pro/Finobe_files/style.css" rel="stylesheet">
-<nav>
+
+<nav class="navbar navbar-default">
 
 </nav>
 
+<div class=".well">
 <h1>Welcome to Revival List List (RLL)</h1>
 <h2>This is a List For All Revival Lists</h2>
+</div>
 
+<style>
+    h1 {
+        text-align: center;
+    }
+    h2 {
+        text-align: center;
+    }
+</style>
