0164ec554febf4efeeeb78bb4e86b54841b5b14d
3a152 <uwuekittenslayer@gmail.com>
2025-11-24 21:36:31 +0700

    added stuff in list

--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -1,6 +1,6 @@
 <div class="intro">
 <h1>The List</h1>
-<h2>you wanted the list right,here you go.</h2>
+<h2>you wanted the list right?, here you go.</h2>
 </div>
 
 <style>
@@ -15,3 +15,25 @@
 
 <hr>
 
+<div class="container my-5">
+    <div class="row row-cols-1 row-cols-md3 g-4">
+
+<!--revival list.com-->
+<div class="col">
+    <div class="card h-100">
+        <img src="/images/revlistcom.png" alt="revival list" class="card-img-top rounded">
+        <div class="card-body">
+          <h5 class="card-title">Roblox Revival Collectors</h5>
+          <p class="card-text">By VandGD</p>
+          <div class="d-flex gap-2">
+          <a href="https://revival-list.com" class="btn btn-primary">Website</a>
+          <a href="https://discord.gg/RqhWsmWatx" class="btn btn-primary">Discord</a>
+          </div>
+        </div>
+    </div>
+</div>
+
+
+
+    </div>
+</div>
