69945afe55a98c49aa5682cef19c50535d132640
3a152 <uwuekittenslayer@gmail.com>
2025-11-25 19:49:51 +0700

    credits done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,3 +12,6 @@
 </style>
 
 <img src="/images/fulllogo.png" alt="full logo" class="d-block mx-auto rounded">
+
+<h5 style="text-align: center; color:white">gifted power: ai</h5>
+<h5 style="text-align: center; color:white">pure effort: this site</h5>
--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -5,3 +5,9 @@
 
 <hr>
 
+<div class="intro">
+ <img src="/images/3a152.png" alt="creator of project">
+ <h1>3a152</h1>
+ <img src="/images/s3dcat.png" alt="cocreator of project">
+ <h1>s3dcat</h1>
+</div>
