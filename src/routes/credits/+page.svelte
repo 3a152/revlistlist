f41a2c0cb205326342128534b3dc827a86dcb1e9
3a152 <uwuekittenslayer@gmail.com>
2025-11-26 19:14:47 +0700

    new stuff

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -6,8 +6,10 @@
 <hr>
 
 <div class="intro">
- <img src="/images/3a152.png" alt="creator of project">
+ <img src="/images/3a152.png" alt="creator of project" width="200">
  <h1>3a152</h1>
- <img src="/images/s3dcat.png" alt="cocreator of project">
+ <h2>Creator of Project</h2>
+ <img src="/images/s3dcat.png" alt="cocreator of project" width="200">
  <h1>s3dcat</h1>
+ <h2>Co-Creator of Project</h2>
 </div>
---
+++ src/routes/gallery/+page.svelte
@@ -0,0 +1,16 @@
+<img src="/images/KARL.png" alt="karl" width="200">
+<br>
+<img src="/images/logo.png" alt="revlistnormal" width="200">
+<br>
+<img src="/images/notfinal.png" alt="initialrevlist" width="200">
+<br>
+<img src="/images/moving.gif" alt="movingrevlist" width="200">
+<br>
+<img src="/images/china.png" alt="china" width="200">
+<br>
+<img src="/images/Chirstmas.jpg" alt="fiobelsit" width="200">
+<br>
+<img src="/images/spookylist.png" alt="scary" width="200">
+<br>
+<img src="/images/Untitled1.png" alt="jolly" width="200">
+<br>
