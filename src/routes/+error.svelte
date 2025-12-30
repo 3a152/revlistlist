a86f7a2ec5f43841895c4bff5fa77d3d104bcefb
3a152 <uwuekittenslayer@gmail.com>
2025-12-30 17:41:26 +0700

    changed error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,7 +5,7 @@
 
 <div class="box">
   <h1>{status}</h1>
-  <p>{error?.message ?? "yeah smth went wrong"}</p>
+  <p>{error?.message ?? "fuck you youre not supposed to be here"}</p>
   <img src="/images/sadblob.png" alt="crying blobmoji" class="d-block mx-auto rounded">
    <a href="/" class="btn btn-success mt-3">go back</a>
 </div>
