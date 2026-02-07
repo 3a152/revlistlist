442a5e554f1db6342fd1ec2dc2d8889727644e61
3a152 <uwuekittenslayer@gmail.com>
2026-02-07 21:46:42 +0700

    change 404 page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,7 +5,7 @@
 
 <div class="box">
   <h1>{status}</h1>
-  <p>{error?.message ?? "fuck you youre not supposed to be here"}</p>
+  <p>{error?.message ?? "how tf are you here???????"}</p>
   <img src="/images/sadblob.png" alt="crying blobmoji" class="d-block mx-auto rounded">
    <a href="/" class="btn btn-success mt-3">go back</a>
 </div>
